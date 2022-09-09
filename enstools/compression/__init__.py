b78f4825df4c3a5f9c6dbdc613073477fe2a28bf
Oriol Tintó <oriol.tinto@lmu.de>
2022-09-09 16:44:51 +0200

    Adding version information in __init__.py

--- enstools/compression/__init__.py
+++ enstools/compression/__init__.py
@@ -4,3 +4,5 @@ from .analyzer.analyzer import analyze
 from .significant_bits import analyze_file_significant_bits
 from .evaluator import evaluate
 from .cli import main as cli
+
+__version__ = "v0.1.0"
