e0075ffe9229be7a43a4f695c302a777649e1a3b
Oriol Tintó <oriol.tinto@lmu.de>
2022-09-12 13:39:20 +0200

    Dependences pointing to the new pypi packages

--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
 """
-Setup file for easycompression
+Setup file for enstools-compression
 """
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# Use the Readme file as long description.
+try:
+    with open("Readme.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+def find_enstools_packages():
+    """
+    Find the packages inside the enstools folder.
+    """
+
+    return [f'enstools.{p}' for p in (find_packages('enstools'))]
+
 
 # perform the actual install operation
 setup(name="enstools-compression",
-      version="0.1.0",
+      version="0.1.2",
       author="Oriol Tintó",
       author_email="oriol.tinto@lmu.de",
-      packages=[f"enstools.compression"],
-      namespace_packages=['enstools'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url="https://github.com/wavestoweather/enstools-compression",
+
+      packages=find_enstools_packages(),
 
       install_requires=[
-          "enstools @ git+https://github.com/wavestoweather/enstools.git",
-          "enstools-encoding @ git+https://github.com/wavestoweather/enstools-encoding.git",
+          "enstools",
+          "enstools-encoding",
           "zfpy",
       ],
       entry_points={
