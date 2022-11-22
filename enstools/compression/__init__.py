bfbf65f39a6eac7a984a8545293c65afb2dbc8f6
Oriol.Tinto <oriol.tinto@physik.uni-muenchen.de>
2022-11-22 14:45:59 +0000

    Revert "Update how the package gets the version to avoid problem with pkg_resources"
    This reverts commit 7253330510ca61864d1d1296b3778be19be8a577

--- enstools/compression/__init__.py
+++ enstools/compression/__init__.py
@@ -1,6 +1,4 @@
-# To have the Version number in a single place, there's a VERSION file in the package root directory.
-# Here we read that file.
-from pathlib import Path
-version_file = Path(__file__).resolve().parent.parent.parent / "VERSION"
-__version__ = version_file.read_text().strip()
-
+# to convert enstools into a namespace package, the version is now listed here and not in the level above
+import pkg_resources  # part of setuptools
+version = pkg_resources.require("enstools-compression")[0].version
+__version__ = version
