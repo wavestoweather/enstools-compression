c3fd6cf774c94727a35488595b6381823d7b7b02
Oriol Tintó <oriol.tinto@lmu.de>
2022-11-22 15:29:18 +0100

    Go back to sphinx_rtd_theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme = "pydata_sphinx_theme"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
