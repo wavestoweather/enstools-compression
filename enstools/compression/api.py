38e159805655c49e9d2a7f577621e8aa7e09cdfa
Oriol Tintó <oriol.tinto@lmu.de>
2022-11-23 16:34:19 +0100

    Remove cli from api.

--- enstools/compression/api.py
+++ enstools/compression/api.py
@@ -3,6 +3,5 @@ from .compressor import compress
 from .analyzer.analyzer import analyze_files, analyze_dataset
 from .significant_bits import analyze_file_significant_bits
 from .evaluator import evaluate
-from .cli import main as cli
 from .emulation import emulate_compression_on_dataset, emulate_compression_on_data_array,\
     emulate_compression_on_numpy_array
