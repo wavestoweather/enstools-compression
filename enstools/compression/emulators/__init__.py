cccce4aa887dd6ed121b20b3873c33198a991d0f
Oriol Tintó <oriol.tinto@lmu.de>
2022-11-25 09:30:16 +0100

    Define default emulator in emulators.__init__.py

--- enstools/compression/emulators/__init__.py
+++ enstools/compression/emulators/__init__.py
@@ -1,3 +1,6 @@
 from .ZFPEmulator import ZFPEmulator
 from .FiltersEmulator import FilterEmulator
 from .LibpressioEmulator import LibpressioEmulator
+
+# Define the default emulator
+default_emulator = FilterEmulator
